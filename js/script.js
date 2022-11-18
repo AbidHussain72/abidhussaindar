0b2b00950fa617c4057f6ddad197a507b2183dfa
AD\Deepak <dekumar@equinix.com>
2022-11-18 14:57:47 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass('sticky');
+        } else {
+            $('.navbar').removeClass('sticky');
+        }
+        if (this.scrollY > 500) {
+            $('.scroll-up-button').addClass('show');
+        } else {
+            $('.scroll-up-button').removeClass('show');
+        }
+    });
+    // slide up script
+    $(".scroll-up-button").click(function() {
+        $('html').animate({ scrollTop: 0 })
+    });
+
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+
+    });
+
+    // typing animation script
+
+    let typed = new Typed(".typing", {
+        strings: ['YouTuber', 'Blogger', 'Designer', 'Freelancer', 'Developer'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    let typedD = new Typed(".typing-2", {
+        strings: ['YouTuber', 'Blogger', 'Designer', 'Freelancer', 'Developer'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            600: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+    
+   
+    });
